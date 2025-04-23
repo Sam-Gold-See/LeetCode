5d4594a58144b3218d4cc265af9d30091d1a172d
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-04-23 20:50:41 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/spring25/Else/DynamicProgrammingTest.java
@@ -0,0 +1,24 @@
+package com.leetcode.spring25.Else;
+
+import org.junit.Test;
+
+public class DynamicProgrammingTest {
+
+/*
+    // 509 斐波那契数 DP
+    public int fib(int n) {
+        if (n < 2)
+            return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+*/
+
+    @Test
+    public void test() {
+    }
+}
