37b54e0301a3d8821659ba21f42dd102a5d9b7d4
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-06 17:39:52 +0800

    LeetCode

--- src/main/java/com/leetcode/spring25/Else/ElseTest.java
+++ src/main/java/com/leetcode/spring25/Else/ElseTest.java
@@ -31,6 +31,17 @@ public class ElseTest {
     }
 */
 
+/*
+    // 1920 基于排列构建数组 模拟
+    public int[] buildArray(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        for(int i = 0;i < len; i++)
+            ans[i] = nums[nums[i]];
+        return ans;
+    }
+*/
+
     @Test
     public void test() {
     }
