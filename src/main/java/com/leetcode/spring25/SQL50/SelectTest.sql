87a6f0d2490b1eca3f78cf5511d1e43de3016b1c
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-09 14:25:40 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/spring25/SQL50/SelectTest.sql
@@ -0,0 +1,14 @@
+# 1757 可回收且低脂的产品
+SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+# 584 寻找用户推荐人
+SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
+
+# 595 大的国家
+SELECT name, population, area FROM World WHERE area >= 3000000 OR population >=25000000;
+
+# 1148 文章浏览 I
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id = viewer_id ORDER BY id ASC;
+
+# 1683 无效的推文
+SELECT tweet_id FROM Tweets WHERE CHAR_LENGTH(content) > 15;
