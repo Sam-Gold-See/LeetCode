b9559b08da2e7ec1ec472f803edd044458c342f5
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-06-01 10:09:52 +0800

    LeetCode

--- src/main/java/com/leetcode/spring25/CodeTopTest.java
+++ src/main/java/com/leetcode/spring25/CodeTopTest.java
@@ -188,6 +188,52 @@ public class CodeTopTest {
     }
 */
 
+/*
+    // 15 三数之和 双指针
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        if (len < 3) return ans;
+        for (int i = 0; i < len - 2; i++) {
+            if (nums[i] > 0)
+                break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1, k = len - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.add(List.of(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
+                    j++;
+                    k--;
+                } else if (sum < 0)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return ans;
+    }
+*/
+
+/*
+    // 53 最大子数组和 贪心
+    public int maxSubArray(int[] nums) {
+        int sum = 0, ans = Integer.MIN_VALUE;
+        for (int num : nums) {
+            sum += num;
+            ans = Math.max(sum, ans);
+            sum = Math.max(sum, 0);
+        }
+        return ans;
+    }
+*/
+
     @Test
     public void test() {
     }
