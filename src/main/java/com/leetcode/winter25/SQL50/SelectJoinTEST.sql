ec507f17717e330d2e7d43b36347515eae4df154
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-12-24 21:56:47 +0800

    LeetCode

--- src/main/java/com/leetcode/winter25/SQL50/SelectJoinTEST.sql
+++ src/main/java/com/leetcode/winter25/SQL50/SelectJoinTEST.sql
@@ -34,3 +34,50 @@ WHERE l1.id = l2.id - 1
 AND l2.id = l3.id - 1
 AND l1.num = l2.num
 AND l2.num = l3.num;
+
+# 1164. 指定日期的产品价格
+
+SELECT p1.product_id, IFNULL(p2.new_price, 10) AS price
+FROM (
+    SELECT DISTINCT product_id
+    FROM Products
+) p1
+LEFT JOIN (
+    SELECT product_id, new_price
+    FROM Products
+    WHERE (product_id, change_date) IN (
+        SELECT product_id, MAX(change_date)
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    )
+) p2
+ON p1.product_id = p2.product_id
+
+# 1204. 最后一个能进入巴士的人
+
+SELECT person_name
+FROM (
+    SELECT turn, person_name, SUM(weight) OVER(ORDER BY turn) AS sum_weight
+    FROM Queue
+) t
+WHERE sum_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1
+
+# 1907. 按分类统计薪水
+
+select 'Low Salary' as category,
+    count(*) as accounts_count
+from accounts
+where income<20000
+union
+select 'Average Salary' as category,
+    count(*) as accounts_count
+from accounts
+where income<=50000 and income>=20000
+union
+select 'High Salary' as category,
+    count(*) as accounts_count
+from accounts
+where income>50000
