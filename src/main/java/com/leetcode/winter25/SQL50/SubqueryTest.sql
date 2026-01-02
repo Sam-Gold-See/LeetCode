89c40be255ed0f4619d149dcdb26886d4b969d52
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2026-01-02 15:47:58 +0800

    LeetCode

--- src/main/java/com/leetcode/winter25/SQL50/SubqueryTest.sql
+++ src/main/java/com/leetcode/winter25/SQL50/SubqueryTest.sql
@@ -31,3 +31,40 @@ LEFT JOIN MovieRating mr ON m.movie_id = mr.movie_id AND YEAR(mr.created_at) = 2
 GROUP BY mr.movie_id
 ORDER BY AVG(mr.rating) DESC, title
 LIMIT 1)
+
+# 1321. 餐馆营业额变化增长
+
+SELECT a.visited_on, SUM( b.amount ) AS amount, ROUND(SUM(b.amount) / 7, 2) AS average_amount
+FROM ( SELECT DISTINCT visited_on FROM customer ) a
+JOIN customer b ON DATEDIFF( a.visited_on, b.visited_on ) BETWEEN 0 AND 6
+WHERE a.visited_on >= (SELECT MIN(visited_on) FROM customer) + 6
+GROUP BY a.visited_on
+ORDER BY a.visited_on
+
+# 602. 好友申请 II ：谁有最多的好友
+
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id id FROM RequestAccepted
+) t1
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
+
+# 585. 2016年的投资
+
+SELECT ROUND(SUM(tiv_2016), 2)AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN(
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(1) > 1)
+AND CONCAT(lat, lon) IN(
+    SELECT CONCAT(lat, lon)
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
