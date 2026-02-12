c76eea00375f7f02898c988f787cab588465c7e5
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2026-02-12 20:20:10 +0800

    LeetCode

--- src/main/java/com/leetcode/winter25/LeetCode100/BinarySearchTest.java
+++ src/main/java/com/leetcode/winter25/LeetCode100/BinarySearchTest.java
@@ -69,9 +69,54 @@ public class BinarySearchTest {
 	}
 */
 
+/*
 	// 33. 搜索旋转排序数组 二分查找
 	public int search(int[] nums, int target) {
+		int n = nums.length;
+		int l = 0, r = n - 1;
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+			if (nums[l] <= nums[mid]) {
+				if (nums[l] <= target && target < nums[mid]) {
+					r = mid - 1;
+				} else {
+					l = mid + 1;
+				}
+			}
+			if (nums[mid] <= nums[r]) {
+				if (nums[mid] < target && target <= nums[r]) {
+					l = mid + 1;
+				} else {
+					r = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
+*/
 
+	// 153. 寻找旋转排序数组中的最小值 二分查找
+	public int findMin(int[] nums) {
+		int n = nums.length;
+		int l = 0, r = n - 1;
+		if (nums[l] <= nums[r]) {
+			return nums[0];
+		}
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+			if (mid > 0 && nums[mid] < nums[mid - 1]) {
+				return nums[mid];
+			}
+			if (nums[mid] >= nums[0]) {
+				l = mid + 1;
+			}else {
+				r = mid - 1;
+			}
+		}
+		return nums[l];
 	}
 
 	@Test
